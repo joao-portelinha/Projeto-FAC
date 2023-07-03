751636a664bbbdfaa942e4374b3aa28a2b4dd0d5
João Portelinha <joaoportelinha@hotmail.com>
2023-07-03 23:50:41 +0100

    Updated Arduino Code

--- codigo_arduino/codigo_arduino.ino
+++ codigo_arduino/codigo_arduino.ino
@@ -1,5 +1,8 @@
 #include <Arduino_HTS221.h>
 #include <Arduino_LPS22HB.h>
+#include <Arduino_APDS9960.h>
+
+int proximity;
 
 void setup() {
   Serial.begin(9600);
@@ -13,32 +16,41 @@ void setup() {
     Serial.println("Failed to initialize pressure sensor!");
     while (1);
   }
+  if (!APDS.begin()) {
+    Serial.println("Error initializing APDS-9960 sensor!");
+  }
 }
 
 void loop() {
    // read all the sensor values
-  float temperature = HTS.readTemperature();
+  float temperature = HTS.readTemperature() - 5;
   float humidity    = HTS.readHumidity();
-  // read the sensor value
   float pressure = BARO.readPressure();
+  if (APDS.proximityAvailable()) {
+    proximity = APDS.readProximity();
+  }
+
 
   // print each of the sensor values
-  Serial.print("Temperature = ");
-  Serial.print(temperature - 5);
+  Serial.print("Temperatura = ");
+  Serial.print(temperature);
   Serial.println(" °C");
 
-  Serial.print("Humidity    = ");
+  Serial.print("Humidade    = ");
   Serial.print(humidity);
   Serial.println(" %");
 
-  // print the sensor value
-  Serial.print("Pressure = ");
+  Serial.print("Pressão = ");
   Serial.print(pressure);
   Serial.println(" kPa");
+
+  Serial.print("Proximidade = ");
+  Serial.println(proximity);  //0   => close; 255 => far; -1  => error
+
 
   // print an empty line
   Serial.println();
 
   // wait 1 second to print again
-  delay(1000);
+  delay(100);
 }
