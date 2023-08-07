2bbbb86addafc9f232c94eecd6cd0c5ccffe7964
Fábio Gonçalves <94992220+FabiomtGoncalves@users.noreply.github.com>
2023-08-07 15:58:11 +0100

    Add files via upload

--- web/app.js
+++ web/app.js
@@ -1,4 +1,5 @@
 
+
 var arr = []
 fetch("output.json")
 .then(response => response.json())
@@ -6,49 +7,38 @@ fetch("output.json")
     console.log(data)
     console.log(data.temperatura)
 
-    document.querySelector("#print_json").innerText = data.temperatura
+    //document.querySelector("#print_json").innerText = data.temperatura
 
-    for (var i=0; i<data.temperatura.length; i++){
-        arr[i] = data.temperatura[i];
-    }
-    console.log(arr)
-})
-
-/*var xmlhttp = new XMLHttpRequest();
-var url = "output.json"
-xmlhttp.open("GET", url, true);
-xmlhttp.send();
-xmlhttp.onreadystatechange = function(){
-    if (this.readyState == 4 && this.status == 200){
-        var data = JSON.parse(this.responseText);
-        var temperature = data.temperatura.map(function(elem){
-            return elem.temperatura
-        });
-
-        console.log(temperature)
-
-    }
-}*/
-
-const ctx = document.getElementById('myChart');
+    const ctx = document.getElementById('myChart');
       
-new Chart(ctx, {
-  type: 'line',
-  data: {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-    datasets: [{
-      label: 'Temperatura (ºC)',
-      data: [arr],
-      backgroundColor: 'transparent',
-      borderColor: 'red',
-      borderWidth: 4
-    }]
-  },
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true
+    new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: Array.from({ length: data.temperatura.length }, (_, i) => `Temperatura ${i + 1}`),
+        datasets: [{
+          label: 'Temperatura (ºC)',
+          data: data.temperatura,
+          backgroundColor: 'transparent',
+          borderColor: 'red',
+          borderWidth: 4
+        }]
+      },
+      options: {
+        tension: 0.6,
+        scales: {
+          x: {
+            display: false 
+        },
+          y: {
+            display: true,
+            title: {
+              display: true,
+              text: 'Temperatura (ºC)',
+            }
+          }
+        }
       }
-    }
-  }
-});
+    });
+    
+
+})
