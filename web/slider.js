6735e1ac0c15ff0c742c1ecc6f204b01be339759
Fábio Gonçalves <94992220+FabiomtGoncalves@users.noreply.github.com>
2023-08-10 23:27:47 +0100

    Add files via upload

--- web/slider.js
+++ web/slider.js
@@ -13,15 +13,24 @@ function currentSlide(n) {
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
+  //let dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";  
   }
-  for (i = 0; i < dots.length; i++) {
+  /*for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
-  }
+  }*/
   slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
+  //dots[slideIndex-1].className += " active";
 }
+
+document.addEventListener('keydown', function(e){
+  if(e.key === "ArrowLeft") {
+    plusSlides(-1);
+  } else if(e.key === "ArrowRight") {
+    plusSlides(1);
+  }
+  
+})
