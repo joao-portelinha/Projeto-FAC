05f36deb8ce2c6f4adcedc225465c407725baf61
FabiomtGoncalves <fabio.mt.goncalves@gmail.com>
2023-08-14 15:33:46 +0100

    buttons, animated background (lag)

--- web/slider.js
+++ web/slider.js
@@ -13,17 +13,12 @@ function currentSlide(n) {
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
-  //let dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";  
   }
-  /*for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }*/
   slides[slideIndex-1].style.display = "block";  
-  //dots[slideIndex-1].className += " active";
 }
 
 document.addEventListener('keydown', function(e){
@@ -34,3 +29,11 @@ document.addEventListener('keydown', function(e){
   }
   
 })
+
+document.getElementById('arrow_left').addEventListener('click', function (e) {
+  plusSlides(-1);
+});
+
+document.getElementById('arrow_right').addEventListener('click', function (e) {
+  plusSlides(1);
+});
